925dc9933506ab2d195f3f76c471871ba6d7755e
121140014-MGunturAlBaihaqi <mguntur.121140014@student.itera.ac.id>
2024-03-27 22:20:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a121140014_pam_latihan6/User.kt
@@ -0,0 +1,9 @@
+package com.example.a121140014_pam_latihan6
+
+data class User(
+    val id: Int,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val avatar: String
+)
